97e54ab88717147ddb4aabb9d053844c04594268
Noel Schenk <schenknoel@gmail.com>
2025-01-24 20:08:28 +0100

    add fix for popup

--- levels/main/mainLevel.gd
+++ levels/main/mainLevel.gd
@@ -7,5 +7,5 @@ func initWindow():
 	var screen_size = DisplayServer.screen_get_size()
 	var window_size = Vector2i(screen_size.x / 2, screen_size.y / 2)
 	get_viewport().size = window_size
-	get_window().position = get_window().position - Vector2i(0, -40)
+	get_window().position = get_window().position - Vector2i(0, -120)
 	#get_window().position = Vector2(screen_size.x / 2 - window_size.x / 2, screen_size.y / 2 - window_size.y / 2)
