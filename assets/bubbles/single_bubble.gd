587b99392ee95f45fa8ae2c6af4acdbfefa2a5a5
Nojoule <nojoule0@gmail.com>
2025-01-25 00:59:10 +0100

    WIP bubble

---
+++ assets/bubbles/single_bubble.gd
@@ -0,0 +1,53 @@
+@tool
+class_name SingleBubble
+extends Sprite2D
+
+
+@export var squish_curve: Curve
+@export var squish_anim_time: float = 4.0
+@export var squish_origin: Vector3 = Vector3(0.0, -1.0, 0.0):
+	set(value):
+		squish_origin = value
+		update_material("squish_origin", value)
+
+@export var grow_curve: Curve
+@export var grow_anim_time: float = 4.0
+@export var grow_origin: Vector3 = Vector3(0.0, -1.0, 0.0):
+	set(value):
+		grow_origin = value
+		update_material("grow_origin", value)
+
+@export var grow = false
+@export_range(0.0, 1.0, 0.01) var grow_status: float = 1.0
+
+@export var squish = false
+@export_range(0.0, 1.0, 0.01) var squish_status: float = 0.0
+
+var _elapsed_squished_time: float = 0.0
+var _elapsed_grow_time: float = 0.0
+
+func update_material(param_name: String, value: Variant) -> void:
+	if material:
+		material.set_shader_parameter(param_name, value)
+
+func _ready() -> void:
+	update_material("squish_origin", squish_origin)
+	update_material("grow_origin", grow_origin)
+	update_material("squish_time", 0.0)
+	update_material("grow_time", 0.0)
+
+
+func _process(delta: float) -> void:
+	if grow:
+		_elapsed_squished_time += delta
+		var squished_normalized_time = clamp(_elapsed_squished_time / squish_anim_time, 0.0, 1.0)
+		update_material("squish_time", squish_curve.sample(squished_normalized_time))
+	else:
+		update_material("squish_time", squish_status)
+
+	if squish:
+		_elapsed_grow_time += delta
+		var grow_normalized_time = clamp(_elapsed_grow_time / grow_anim_time, 0.0, 1.0)
+		update_material("grow_time", grow_curve.sample(grow_normalized_time))
+	else:
+		update_material("grow_time", grow_status)
