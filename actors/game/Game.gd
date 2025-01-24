835805fd98e565623e61226f4e9507ff0a4d1db3
Jan Sobotta <cv.jsobotta@googlemail.com>
2025-01-24 18:29:11 +0100

    add game singleton

--- actors/game/Game.gd
+++ actors/game/Game.gd
@@ -1,13 +1,16 @@
 extends Node2D
+class_name Game
 
+static var instance: Game
+static func getGame():
+	return instance
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	Game.instance = self
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func getSpawnPoints():
