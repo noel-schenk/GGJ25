cffb85bb1719861ca403855a2cb95213daa42d65
Jan Sobotta <cv.jsobotta@googlemail.com>
2025-01-25 18:18:51 +0100

    bubble

--- actors/bubbles/bubble_normal/bubble_normal.gd
+++ actors/bubbles/bubble_normal/bubble_normal.gd
@@ -16,8 +16,16 @@ func _ready() -> void:
 	
 func pop():
 	collision_mask = 0
+	collision_layer = 0
 	if multiplayer.is_server():
 		base.pop()
 		Utils.set_timeout(func():
 			queue_free()
 		, 1.0)
+
+func foamify():
+	collision_mask = 0
+	collision_layer = 0
+	if multiplayer.is_server():
+		base.pop()
+	
