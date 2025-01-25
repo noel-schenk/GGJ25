452fe2bb4c3713daefd80c894928dcaf523bf4a0
Nojoule <nojoule0@gmail.com>
2025-01-25 21:09:16 +0100

    bubble move animation resets smoothly after moving

--- actors/bubbles/BubbleBase.gd
+++ actors/bubbles/BubbleBase.gd
@@ -1,6 +1,11 @@
 extends Node2D
 class_name BubbleBase
 
+var playing_move = false
+var playing_move_time = 0.0
+var reset_move = false
+var reset_squish_status = 0.0
+
 @onready var AnimatedBubble = $AnimatedBubble
 @onready var Prewiew = $Preview as Sprite2D
 
@@ -27,7 +32,6 @@ func squish(direction: Vector2, speed = 1.0):
 	if !multiplayer.is_server():
 		return
 	var dir = Vector3(direction.x, direction.y, 0)
-	AnimatedBubble.squish_origin = dir.normalized()
 	playAnimation.rpc('move', dir.normalized())
 	
 
@@ -36,4 +40,23 @@ func playAnimation(animation: String, origin: Vector3 = Vector3.ZERO):
 	var ap = AnimatedBubble.get_child(0) as AnimationPlayer
 	if (animation == 'move'):
 		AnimatedBubble.squish_origin = origin
+		playing_move = true
+		playing_move_time = 0.0
 	ap.play(animation)
+
+func _process(delta: float) -> void:
+	if playing_move:
+		playing_move_time += delta
+		if playing_move_time > 0.5:
+			playing_move = false
+			playing_move_time = 0.0
+			reset_move = true
+			reset_squish_status = AnimatedBubble.squish_status
+			var ap = AnimatedBubble.get_child(0) as AnimationPlayer
+			ap.stop()
+			AnimatedBubble.squish_status = reset_squish_status
+	if reset_move:
+		playing_move_time += delta
+		AnimatedBubble.squish_status = lerp(reset_squish_status, 0.0, playing_move_time / 0.5)
+		if playing_move_time > 0.5:
+			reset_move = false
