976c0320a54131031b7e31cc6b76e31ea62f8040
Nojoule <nojoule0@gmail.com>
2025-01-25 21:35:36 +0100

    bubble normal respawn after 7 seconds at original location

--- actors/bubbles/bubble_normal/bubble_normal.gd
+++ actors/bubbles/bubble_normal/bubble_normal.gd
@@ -4,9 +4,14 @@ class_name BubbleNormal
 @export var Pushable = true
 @export var Breakable = true
 
+var original_position = Vector2.ZERO
+var original_collision_mask = 0
+var original_collision_layer = 0
+
 @onready var base = $BubbleBase as BubbleBase
 
 func _ready() -> void:
+	original_position = global_position
 	add_to_group("Bubble")
 	if Pushable:
 		add_to_group("Pushable")
@@ -15,17 +20,15 @@ func _ready() -> void:
 	
 	
 func pop():
+	original_collision_mask = collision_mask
+	original_collision_layer = collision_layer
 	collision_mask = 0
 	collision_layer = 0
 	if multiplayer.is_server():
 		base.pop()
 		Utils.set_timeout(func():
-			queue_free()
-		, 1.0)
-
-func foamify():
-	collision_mask = 0
-	collision_layer = 0
-	if multiplayer.is_server():
-		base.pop()
-	
+			global_position = original_position
+			base.AnimatedBubble.grow = true
+			collision_mask = original_collision_mask
+			collision_layer = original_collision_layer
+		, 7.0)
--- assets/bubbles/single_bubble.gd
+++ assets/bubbles/single_bubble.gd
@@ -24,6 +24,7 @@ extends Sprite2D
 		update_material("grow_origin", value)
 @export var grow = false:
 	set(value):
+		pop_status = 0.0
 		grow = value
 		if value:
 			_elapsed_grow_time = 0.0
