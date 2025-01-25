6bc5d067de2f5122099bbc7e2dcd5287089a6359
Jan Sobotta <cv.jsobotta@googlemail.com>
2025-01-25 04:04:37 +0100

    wizard pull!

--- actors/main/MainCharacter.gd
+++ actors/main/MainCharacter.gd
@@ -10,27 +10,32 @@ const JUMP_VELOCITY = -500.0
 @onready var camera = $Camera2D
 @onready var sprite = $Sprite2D
 
+var activeAction = null
 var currentCamera = null;
+var currentMousePosition = Vector2.ZERO
 
 func _ready() -> void:
 	add_to_group("Player")
 
+
 func _process(_delta: float) -> void:
 	if id == multiplayer.get_unique_id():
+		if(activeAction != null):
+			callAction.rpc('updateSkill', [activeAction, getGlobalMousePos()])
 		callAction.rpc('jump', Input.is_action_just_pressed("ui_accept"))
 		setRemoteDirection.rpc(Input.get_axis("ui_left", "ui_right"))
-		if(currentCamera != camera):
+		if (currentCamera != camera):
 			camera.enabled = true
-			currentCamera = camera;
+			currentCamera = camera
 	
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	var isJumping = remoteJumping
-	if isJumping and is_on_floor():
+	if remoteJumping and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -40,7 +45,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
+		
 	if (direction > 0):
 		sprite.scale.x = -abs(sprite.scale.x)
 	if (direction < 0):
@@ -49,17 +54,56 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseMotion:
+		currentMousePosition = event.position
+	elif event is InputEventKey:
+		var code = event.as_text_keycode()
+		if code in ['1', '2', '3', '4', '5']:
+			if code == activeAction and event.is_released():
+				activeAction = null
+				callAction.rpc('endSkill', [code, getGlobalMousePos()])
+			elif code != activeAction and event.is_pressed():
+				activeAction = code
+				callAction.rpc('startSkill', [code, getGlobalMousePos()])
+
+
 @rpc('any_peer', 'call_local', 'unreliable')
 func setRemoteDirection(direction):
 	if multiplayer.is_server():
 		remoteDirection = direction
+
 
 @rpc('any_peer', 'call_local', 'reliable')
 func callAction(action: String, parameters):
 	if multiplayer.is_server():
 		performAction(action, parameters)
 
+
 func performAction(action: String, parameters):
 	match action:
 		'jump':
 			remoteJumping = bool(parameters)
+		'startSkill':
+			startSkill(parameters[0], parameters[1])
+		'endSkill':
+			endSkill(parameters[0], parameters[1])
+		'updateSkill':
+			updateSkill(parameters[0], parameters[1])
+
+
+func startSkill(skill: String, target: Vector2):
+	pass
+	
+func endSkill(skill: String, target: Vector2):
+	pass
+	
+func updateSkill(skill: String, target: Vector2):
+	pass
+	
+func getGlobalMousePos(pos = currentMousePosition):
+	return get_global_transform_with_canvas().affine_inverse() * pos
+
+func getGlobalCharPos():
+	return get_global_transform().affine_inverse() * global_position
