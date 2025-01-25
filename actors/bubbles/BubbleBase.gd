88690e8f91297fb2868d61730010b503fecb2c3d
Jan Sobotta <cv.jsobotta@googlemail.com>
2025-01-25 15:31:30 +0100

    added bubble teleport

---
+++ actors/bubbles/BubbleBase.gd
@@ -0,0 +1,30 @@
+extends Node2D
+class_name BubbleBase
+
+@onready var AnimatedBubble = $AnimatedBubble
+@onready var Prewiew = $Preview as Sprite2D
+
+func _ready() -> void:
+	AnimatedBubble.pop_status = 0.0
+	AnimatedBubble.grow = true
+	AnimatedBubble.grow_origin = Vector3.ZERO
+	Prewiew.visible = false
+
+func pop():
+	if !multiplayer.is_server():
+		return
+	playAnimation.rpc('pop')
+	
+func bounce(direction: Vector2, speed = ):
+	if !multiplayer.is_server():
+		return
+	var dir = Vector3(direction.x, direction.y, 0)
+	AnimatedBubble.squish_origin = dir.normalized()
+	playAnimation.rpc('bounce')
+	pass
+	
+
+@rpc("authority", "call_local", "reliable")
+func playAnimation(animation: String):
+	var ap = AnimatedBubble.get_child(0) as AnimationPlayer
+	ap.play(animation)
--- actors/bubbles/bubble_teleport/bubble_teleport.gd
+++ actors/bubbles/bubble_teleport/bubble_teleport.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
-@onready var area = $Area2D
+@onready var area = $Area2D as Area2D
+@onready var base = $BubbleBase as BubbleBase
+@onready var innerParticles = $InnerParticles as GPUParticles2D
+@onready var borderParticles = $BorderParticles as GPUParticles2D
 
 func _ready() -> void:
 	add_to_group("Bubble")
 	area.body_entered.connect(onEntered)
 	
+func teleported():
+	area.collision_mask = 0
+	innerParticles.emitting = false
+	borderParticles.emitting = false
+	if multiplayer.is_server():
+		base.pop()
+		Utils.set_timeout(func():
+			queue_free()
+		, 1.0)
 
 func onEntered(body):
 	if body.is_in_group('Player'):
@@ -14,7 +26,7 @@ func onEntered(body):
 		if (bubbles.size() == 2):
 			var bubble = bubbles[0] if bubbles[0] != self else bubbles[1]
 			body.set_global_position(bubble.get_global_position())
-			bubbles[0].queue_free()
-			bubbles[1].queue_free()
+			bubbles[0].teleported()
+			bubbles[1].teleported()
 		else:
 			print_debug('no bubble to teleport to')
